accdc3a11d83fd999f6a0b9fc54428b4ef7967a3
sebastian-lucjan <sebastian.lucjan@gmail.com>
2021-12-17 13:25:34 +0100

    Add exceptions to eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -39,6 +39,9 @@ module.exports = {
       { usePrettierrc: true },
     ], // Use our .prettierrc file as source
     'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': 'off',
+    'no-undef': 'off',
+    'react/no-unescaped-entities': 'off',
     'jsx-a11y/click-events-have-key-events': 'off',
     'jsx-a11y/no-static-element-interactions': 'off',
     'jsx-a11y/img-redundant-alt': 'off',
